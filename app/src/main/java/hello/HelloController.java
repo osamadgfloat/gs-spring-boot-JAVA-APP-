d0e7204e8adb5ebdb8d8165f68ad3a24a0cf253a
OsamaSaeed <osama.saeed@digifloat.com>
2023-02-27 12:06:19 +0500

    Some Updates

--- app/src/main/java/hello/HelloController.java
+++ app/src/main/java/hello/HelloController.java
@@ -8,6 +8,6 @@ public class HelloController {
     
     @RequestMapping("/")
     public String index() {
-        return "Hello from Spring Boot!";
+        return "Hello This is Our First Azure Devops Pipelines 'Welcome To The Java World '";
     }   
 }
